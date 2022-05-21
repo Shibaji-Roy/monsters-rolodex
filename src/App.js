95ffa4d035d42f32bdeb9baec581c49d452c2864
Shibaji Roy <shibajiroy304@gmail.com>
2022-05-21 11:01:03 +0530

    React Project (Monster-rolodex)

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: {firstName: 'Yihua', lastName: 'Zhang'},
-      company: 'ZTM'
+      monsters: [],
+      searchField: "",
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: {firstName: 'Andrei', lastName: 'Neaogie'},
-              }
-            }, () => {
-              console.log(this.state);
-            });
-          }}>Change Name</button>
-        </header>
+        <SearchBox
+          className = 'monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder = 'search monsters'
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
